8455caa97361166a5cb0216da00435bfb22fc627
AndreaMunarin00 <879607@stud.unive.it>
2021-12-19 18:39:08 +0100

    Tasto activity
    aggiunto tasto per creare activity

--- client/app/src/main/java/com/balckbuffalos/familiesshareextended/Fragments/ActivitiesGroupFragment.java
+++ client/app/src/main/java/com/balckbuffalos/familiesshareextended/Fragments/ActivitiesGroupFragment.java
@@ -1,5 +1,6 @@
 package com.balckbuffalos.familiesshareextended.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,17 +9,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.balckbuffalos.familiesshareextended.ActivitiesCreationActivity;
+import com.balckbuffalos.familiesshareextended.LoginActivity;
 import com.balckbuffalos.familiesshareextended.R;
+import com.balckbuffalos.familiesshareextended.SplashScreenActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ActivitiesGroupFragment extends Fragment {
 
-
+    FloatingActionButton createActivity;
     public ActivitiesGroupFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_activities_group, container, false);
+        View view = inflater.inflate(R.layout.fragment_activities_group, container, false);
+        createActivity = view.findViewById(R.id.floating_activity_button);
+        createActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ActivitiesCreationActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
