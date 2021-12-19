5543ca91eeced9afa7dc9d32faa7abb636125e95
AndreaMunarin00 <879607@stud.unive.it>
2021-12-19 15:59:31 +0100

    Create alcune activity
    create alcune activity

---
+++ client/app/src/main/java/com/balckbuffalos/familiesshareextended/ActivitiesCreationActivity.java
@@ -0,0 +1,14 @@
+package com.balckbuffalos.familiesshareextended;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitiesCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities_creation);
+    }
+}
---
+++ client/app/src/main/java/com/balckbuffalos/familiesshareextended/GroupCreationActivity.java
@@ -0,0 +1,14 @@
+package com.balckbuffalos.familiesshareextended;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_creation);
+    }
+}
