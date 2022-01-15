006ab4ff4d348f293f9a0653d42c4b91614d304d
AndreaMunarin00 <879607@stud.unive.it>
2022-01-15 14:43:00 +0100

    profile info fixed

--- client/app/src/main/java/com/balckbuffalos/familiesshareextended/Retrofit/RetrofitClient.java
+++ client/app/src/main/java/com/balckbuffalos/familiesshareextended/Retrofit/RetrofitClient.java
@@ -9,7 +9,7 @@ public class RetrofitClient {
     private static Retrofit instance;
 
     public static Retrofit getInstance() {
-        String IPV4 = "192.168.1.20";
+        String IPV4 = "192.168.1.7";
         String port="4000";
         if (instance == null)
             instance = new Retrofit.Builder()
